1a7b76e31e6e792e7d479d1064465bf578ae64fe
mschw90 <Mschw90@gmail.com>
2017-10-31 18:46:52 -0700

    log and register page

--- reactApp/app.js
+++ reactApp/app.js
@@ -1,19 +1,38 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import EditText from './components/EditText';
+// import EditText from './components/EditText';
 
-// import MyInput from './Components/MyInput';
+import Register from './components/Register';
+// import EditText from './components/EditText';
+// import LandingPage from './components/LandingPage';
 
 /* This can check if your electron app can communicate with your backend */
 // fetch('http://localhost:3000')
 // .then(resp => resp.text())
 // .then(text => console.log(text))
 // .catch(err => {throw err})
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    };
+  }
+
+  render() {
+    return(
+      <div>
+        <Register />
+        {/* <EditText /> */}
+        {/* <LandingPage /> */}
+      </div>
+    );
+  }
+}
 
 ReactDOM.render(
   <MuiThemeProvider>
-
-    <EditText />
+    <App/>
   </MuiThemeProvider>,
    document.getElementById('root'));
--- reactApp/components/LandingPage.js
+++ reactApp/components/LandingPage.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import TextField from 'material-ui/TextField';
+import RaisedButton from 'material-ui/RaisedButton';
+
 
 class LandingPage extends React.Component {
   constructor(props) {
@@ -8,8 +11,34 @@ class LandingPage extends React.Component {
     };
   }
   render() {
+    const style = {
+      margin: 12,
+    };
+
+
+    console.log("anything");
     return (
-        <div>Hey</div>
+      <div className="videoBackground">
+        <video autoPlay loop id="video-background" height="100%" width="auto">
+          <source src="http://localhost:8080/peopleWalking.mp4" type="video/mp4"></source>
+        </video>
+          <div className="loginPage">
+            <div className="header"><h1>HORIZON DOCS</h1></div>
+            <div className="loginBox">
+              <TextField
+                hintText="User Name"
+                floatingLabelText="User Name"
+              /><br />
+              <TextField
+                hintText="Password"
+                floatingLabelText="Password"
+                type="password"
+              /><br />
+              <RaisedButton label="Sign In" primary={true} style={style} />
+              <RaisedButton label="Register" secondary={true} style={style} />
+            </div>
+        </div>
+     </div>
     );
   }
 }
